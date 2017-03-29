55c567844750930a7367cb9e581fb38dd6dc5d3e
MKS-Student <mks.student@hackreactor.com>
2017-03-29 16:06:40 -0700

    Implemented minor diagonal conflict detection
    Minor diagonal tests passing

--- src/Board.js
+++ src/Board.js
@@ -149,19 +149,29 @@
       return false;
     },
 
-
-
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var diagonalSum = 0;
+
+      for (var i = 0; i < this.attributes.n; i++) {
+        if (this._isInBounds(i, minorDiagonalColumnIndexAtFirstRow - i) ) {
+          diagonalSum += this.rows()[i][minorDiagonalColumnIndexAtFirstRow - i];
+        }
+      }
+      return ( diagonalSum > 1 );
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = 1; i < this.attributes.n + 2; i++) {
+        if ( this.hasMinorDiagonalConflictAt(i) ) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
